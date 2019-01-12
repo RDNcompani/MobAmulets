18fdb27bd96fae019582ddb2629143de573490db
danil <danillompas2007@gmail.com>
2019-01-12 17:22:28 +0800

    Добавлено описание для паучих алуметов.

--- src/main/java/com/example/examplemod/Cave_spider.java
+++ src/main/java/com/example/examplemod/Cave_spider.java
@@ -1,9 +1,21 @@
 package com.example.examplemod;
 
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+
+import java.util.List;
+
 public class Cave_spider extends Spider1 {
     public Cave_spider(){
         setTextureName("examplemod:Spider_amulet1");
         setUnlocalizedName("амулет пещерного паука");
+
+    }
+
+    @Override
+    public void addInformation(ItemStack p_77624_1_, EntityPlayer p_77624_2_, List p_77624_3_, boolean p_77624_4_) {
+        super.addInformation(p_77624_1_, p_77624_2_, p_77624_3_, p_77624_4_);
+        p_77624_3_.add("А также даёт защиту от яда");
 
     }
 }
--- src/main/java/com/example/examplemod/Spider1.java
+++ src/main/java/com/example/examplemod/Spider1.java
@@ -3,13 +3,21 @@ package com.example.examplemod;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.TickEvent;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+
+import java.util.List;
 
 public class Spider1 extends Item {
     public Spider1() {
         setTextureName("examplemod:spider_amulet");
         setUnlocalizedName("Паучий амулет");
         setCreativeTab(CreativeTabs.tabMaterials);
+    }
+    @Override
+    public void addInformation(ItemStack p_77624_1_, EntityPlayer p_77624_2_, List p_77624_3_, boolean p_77624_4_) {
+        p_77624_3_.add("Позволяет карабкаться 5 секунд");
     }
 
     @SubscribeEvent
